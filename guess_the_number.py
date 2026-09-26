e22c42f56ede238d2bae30d8cc61b109becefbec
Vishal Kashyap <vishalkashyap4511@gmail.com>
2026-09-26 12:40:33 +0530

    build 'Guess the Number' Game

---
+++ guess_the_number.py
@@ -0,0 +1,31 @@
+# import random as rm
+
+# n = 0
+# while n != 7:
+#     n = rm.randint(1, 10)
+#     print(n)
+
+
+import random as vishal
+
+num = vishal.randint(11, 50)
+user_value = 0
+user_attempt = 0
+
+while num != user_value:
+    print("Please, enter a number (1 to 50): ")
+    user_value = int(input())
+    
+    user_attempt += 1
+    
+    if user_value == num:
+        print(f"Great! You guess the number in {user_attempt}TH Attempt")
+        
+    elif user_value > num:
+        print(f"Value is little bit Higher, Try Again...")
+        
+    elif user_value < num:
+        print(f"Value is little bit Lower, Try Again...")
+        
+    else:
+        print("Invalid!")    
--- tutorial.py
+++ tutorial.py
@@ -288,8 +288,7 @@
 # rev = 0
 # while n > 0:
 #     rev = rev * 10 + n % 10
-#     n = n // 10
-    
+#     n = n // 10    
 # print(rev)  
 
 # if reference == rev:
